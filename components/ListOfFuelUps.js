d0864a0b8b714c6090782e6183fd7c8d74d4f410
Adam Hacsko <adam.hacsko@evosoft.com>
2022-01-15 09:48:30 +0100

    working ui

--- components/ListOfFuelUps.js
+++ components/ListOfFuelUps.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import AddNew from "./AddNew";
 import CustomComponent from "./CustomComponent";
-import ActionButton from "react-native-action-button";
 
 import {
   View,
@@ -58,29 +57,6 @@ export default class ListOfFuelUps extends Component {
             PreviousFuelUp={i != 0 ? array[i - 1] : undefined}
           />
         ))}
-        <ActionButton buttonColor="rgba(231,76,60,1)">
-          <ActionButton.Item
-            buttonColor="#9b59b6"
-            title="New Task"
-            onPress={() => console.log("notes tapped!")}
-          >
-            <Icon name="md-create" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item
-            buttonColor="#3498db"
-            title="Notifications"
-            onPress={() => {}}
-          >
-            <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item
-            buttonColor="#1abc9c"
-            title="All Tasks"
-            onPress={() => {}}
-          >
-            <Icon name="md-done-all" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-        </ActionButton>
       </View>
     );
   }
