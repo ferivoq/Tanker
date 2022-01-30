d28ec737de93e2f548591ffa6bcd6d014a624e7e
Adam Hacsko <adam.hacsko@evosoft.com>
2022-01-30 11:10:58 +0100

    lista working on

--- components/CustomComponent.js
+++ components/CustomComponent.js
@@ -7,6 +7,7 @@ import {
   Platform,
   TextInput,
   StyleSheet,
+  Image,
 } from "react-native";
 
 export default class CustomComponent extends Component {
@@ -36,28 +37,34 @@ export default class CustomComponent extends Component {
 
   render() {
     return (
-      <View style={styles.top}>
-        <Text style={styles.text}>
+      <View style={(styles.container, { flexDirection: "row" })}>
+        <View style={{ flex: 1 }}>
+          <Image style={styles.icon} source={require("../assets/gas.png")} />
+        </View>
+        <View style={{ flex: 2 }}>
+          <Text style={styles.typeText}>
+            {this.props.FuelUp.typeof == "fuelUp" ? "Tankolás" : "Kocsimosás"}
+          </Text>
+          <Text style={styles.normalText}>{this.props.FuelUp.place}</Text>
+          <Text style={styles.normalText}>{this.props.FuelUp.mileage} km</Text>
+        </View>
+        <View style={{ flex: 2, backgroundColor: "green" }} />
+        {/* <Text style={styles.text}>
           {this.props.FuelUp.price
             .toString()
             .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
           Ft
         </Text>
-
         <Text style={styles.text}>
           {this.props.FuelUp.mileage
             .toString()
             .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}{" "}
           km
         </Text>
-
         <Text style={styles.text}>{this.props.FuelUp.volume} l</Text>
-
         <Text style={styles.text}>{this.km} km</Text>
-
         <Text style={styles.text}>{this.pricePerKm} Ft/km</Text>
-
-        <Text style={styles.text}>{this.consuption} l/100km</Text>
+        <Text style={styles.text}>{this.consuption} l/100km</Text> */}
       </View>
     );
   }
@@ -65,19 +72,33 @@ export default class CustomComponent extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
-    alignItems: "center",
-    // justifyContent: "top",
-  },
-  top: {
     backgroundColor: "#6c757d",
     borderRadius: 20,
     width: 400,
     marginBottom: 10,
     padding: 10,
+    flex: 1,
+    padding: 20,
+    marginTop: 10,
   },
   text: {
     color: "#fff",
     fontSize: 25,
+  },
+  typeText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+    marginTop: 10,
+  },
+  normalText: { fontSize: 16 },
+  icon: {
+    width: 50,
+    height: 50,
+    marginTop: 15,
+    marginBottom: 15,
+    marginLeft: 10,
+    backgroundColor: "#5cb075",
+    borderRadius: 7,
   },
 });
--- components/ListOfFuelUps.js
+++ components/ListOfFuelUps.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import AddNew from "./AddNew";
 import CustomComponent from "./CustomComponent";
-import { Alert } from "react-native";
+import { Alert, ScrollView } from "react-native";
 import * as Rootnavigation from "../RootNavigation";
 
 import {
@@ -17,29 +17,64 @@ import { RootTagContext } from "react-native/Libraries/ReactNative/RootTag";
 const fuelUp = [
   {
     key: 1,
-    date: "2021.08.31",
-    mileage: 189790,
+    typeof: "fuelUp",
+    place: "Shell",
+    date: "2021.05.23",
+    mileage: 51800,
     volume: 36.06,
     price: 15755,
   },
   {
     key: 2,
-    date: "2021.09.05",
-    mileage: 190164,
+    typeof: "fuelUp",
+    place: "Shell",
+    date: "2021.05.21",
+    mileage: 51800,
     volume: 33.12,
     price: 15035,
   },
   {
     key: 3,
-    date: "2021.09.13",
-    mileage: 190571,
+    typeof: "fuelUp",
+    place: "Shell",
+    date: "2021.04.23",
+    mileage: 51800,
     volume: 35.26,
     price: 15793,
   },
   {
     key: 4,
-    date: "2021.09.28",
-    mileage: 190947,
+    typeof: "fuelUp",
+    place: "Shell",
+    date: "2021.04.30",
+    mileage: 51800,
+    volume: 33.53,
+    price: 14000,
+  },
+  {
+    key: 4,
+    typeof: "asd",
+    place: "Shell",
+    date: "2021.03.1",
+    mileage: 51800,
+    volume: 33.53,
+    price: 14000,
+  },
+  {
+    key: 4,
+    typeof: "fuelUp",
+    place: "Shell",
+    date: "2021.2.24",
+    mileage: 51800,
+    volume: 33.53,
+    price: 14000,
+  },
+  {
+    key: 4,
+    typeof: "fuelUp",
+    place: "Shell",
+    date: "2021.2.12",
+    mileage: 51800,
     volume: 33.53,
     price: 14000,
   },
@@ -63,7 +98,7 @@ export default class ListOfFuelUps extends Component {
   render() {
     var that = this;
     return (
-      <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.container}>
         {/* <Button
           title={"3-Button Alert"}
           onPress={() => Rootnavigation.navigate("AddNew")}
@@ -75,7 +110,7 @@ export default class ListOfFuelUps extends Component {
             PreviousFuelUp={i != 0 ? array[i - 1] : undefined}
           />
         ))}
-      </View>
+      </ScrollView>
     );
   }
 }
